442198e8a75322f860b3aa0bb12a2add04a8beff
EFRA <efra030392@gmail.com>
2026-09-07 20:15:55 -0400

    Configuración de PostgreSQL con TypeORM en aplicaciones NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module.js';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Env } from './config/env.model.js';
 
 @Module({
   imports: [
@@ -8,6 +10,23 @@ import { UsersModule } from './users/users.module.js';
     // usar variables de entorno en toda la aplicacion
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      // Estamos usando el metodo forRootAsync para poder usar 
+      // variables de entorno en la configuracion de TypeORM
+      useFactory: (configService: ConfigService<Env>) => ({
+      type: 'postgres',
+      host: configService.get('POSTGRES_HOST', { infer: true }),
+      port: configService.get('POSTGRES_PORT', { infer: true }),
+      username: configService.get('POSTGRES_USER', { infer: true }),
+      password: configService.get('POSTGRES_PASSWORD', { infer: true }),
+      database: configService.get('POSTGRES_DB', { infer: true }),
+      autoLoadEntities: true,
+      synchronize: true,
+      }),
+      // Estamos inyectando el servicio de configuracion 
+      // para poder usar
+      inject: [ConfigService],
     }),
     UsersModule,
   ],
